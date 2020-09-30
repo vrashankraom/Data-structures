214000920a4b7b37c08121fed361e856bfd072c8
M VISHWAS KAMATH <vishwask.cs18@sahyadri.edu.in>
2020-09-30 23:36:35 +0530

    Update Fibonacci_DP.cpp
    Improved the code for Fibonacci using dynamic programming

--- Fibonacci_DP.cpp
+++ Fibonacci_DP.cpp
@@ -1,22 +1,29 @@
-#include<iostream>
-using namespace std;
-int fib(int n,int *dp){
-     if(n<=1)
-        return n;
-     //dynamic programming
-      if(dp[n]!=-1)
-        return dp[n];
-      
-      dp[n]=fib(n-1,dp)+fib(n-2,dp);
-      return dp[n];
-}
-int main(){
-  int n;
-  cin>>n;
-  //for numbers from 0 to n
-  int dp[n+1];
-  for(int i=0;i<=n;i++)
-      dp[i]=-1;
-  cout<<fib(n,dp);
-  return 0;
-}
+//Fibonacci Series using Dynamic Programming 
+#include<stdio.h> 
+
+int fib(int n) 
+{ 
+
+int f[n+2]; // 1 extra to handle case, n = 0 
+int i; 
+
+
+f[0] = 0; 
+f[1] = 1; 
+
+for (i = 2; i <= n; i++) 
+{ 
+
+	f[i] = f[i-1] + f[i-2]; 
+} 
+
+return f[n]; 
+} 
+
+int main () 
+{ 
+int n = 9; 
+printf("%d", fib(n)); 
+getchar(); 
+return 0; 
+} 
