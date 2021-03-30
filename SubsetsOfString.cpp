658f6e96f082f09adf8819a252b7dfaa97609d39
Vrashank Rao M <vrashank.cs18@sahyadri.edu.in>
2021-03-30 21:37:33 +0530

    Create SubsetsOfString.cpp

---
+++ SubsetsOfString.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+int count;
+using namespace std;
+
+
+
+void subsets(char *a,int i,int j,char *temp){
+    if(a[i]=='\0'){
+        temp[j]='\0';
+        cout<<temp<<endl;
+        count = count;
+        return;
+    }
+    
+    temp[j]=a[i];
+    subsets(a,i+1,j+1,temp);
+    subsets(a,i+1,j,temp);
+}
+
+int main()
+{
+    char a[100];
+    cin>>a;
+    char temp[100];
+    subsets(a,0,0,temp);
+
+    return 0;
+}
