e90913a2d83d7f5a692852fa5c20fb96e51f0b0e
Vrashank Rao M <vrashank.cs18@sahyadri.edu.in>
2021-04-24 21:21:38 +0530

    Create postfix&prefixEvaluation.cpp

---
+++ postfix&prefixEvaluation.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int postfixEvaluation(string s){
+    stack<int>st;
+    for(int i=0;i<s.size();i++){
+        if(s[i]>='0' && s[i]<='9'){
+            st.push(s[i]-'0');
+        }
+        else{
+            int op2=st.top();
+            st.pop();
+            int op1=st.top();
+            st.pop();
+            
+            switch(s[i]){
+                case '+': st.push(op1+op2);
+                break;
+                case '-': st.push(op1-op2);
+                break;
+                case '*': st.push(op1*op2);
+                break;
+                case '/': st.push(op1/op2);
+                break;
+                case '^': st.push(pow(op1,op2));
+                break;
+                default:return -9999;
+            }
+        }
+    }
+    return st.top();
+}
+
+int main()
+{
+    string s;
+    cin>>s;
+    int result=postfixEvaluation(s);
+    cout<<result;
+    return 0;
+}
