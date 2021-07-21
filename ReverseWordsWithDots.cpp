9bf57941f17428c70e8eb3325981e8fec10e4835
Vrashank Rao <vrashank.cs18@sahyadri.edu.in>
2021-07-21 20:18:06 +0530

    Update ReverseWordsWithDots.cpp

--- ReverseWordsWithDots.cpp
+++ ReverseWordsWithDots.cpp
@@ -1,3 +1,52 @@
+//No need of vector
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string reverseWord(string temp){
+    int start=0;
+    int end = temp.size()-1;
+    
+    while(start<end){
+        swap(temp[start],temp[end]);
+        start++;
+        end--;
+    }
+    return temp;
+}
+
+void reverseStringWithDots(string s){
+    string temp="";
+    string result="";
+    
+    for(int i=s.size()-1;i>=0;i--){
+        if(s[i]=='.'){
+            result = result+reverseWord(temp);
+            result=result+'.';
+            temp="";
+        }
+        else{
+        temp = temp+s[i];
+        }
+    }
+    result = result+reverseWord(temp);
+    cout<<result;
+    
+}
+
+int main()
+{
+    string s="hello.man.im.vrashank";
+    //getline(cin,s);
+    reverseStringWithDots(s);
+    return 0;
+}
+
+
+
+
+
 
 //we use vectors here and push back all the words.then print all the words int reverse
 #include <bits/stdc++.h> 
