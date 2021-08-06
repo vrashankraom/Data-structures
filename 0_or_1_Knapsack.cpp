ab699909e214d53b9c4f086036556071a3f7eb57
Vrashank Rao <vrashank.cs18@sahyadri.edu.in>
2021-08-06 23:55:17 +0530

    Update 0_or_1_Knapsack.cpp

--- 0_or_1_Knapsack.cpp
+++ 0_or_1_Knapsack.cpp
@@ -42,3 +42,32 @@ int main()
     cout << knapSack(W, wt, val, n);
     return 0;
 }
+
+
+//using DP
+class Solution
+{
+    public:
+    
+    int findKnapSack(int W, int wt[], int val[], int n,vector<vector<int>>&dp){
+        if(n==0 || W==0){
+          return 0;
+      }
+      
+      if(dp[n-1][W]!=-1){
+          return dp[n-1][W];
+      }
+      if (wt[n - 1] > W)
+        return findKnapSack(W, wt, val, n - 1,dp);
+      
+      int steal = val[n-1] + findKnapSack(W-wt[n-1],wt,val,n-1,dp);
+      int notsteal = findKnapSack(W,wt,val,n-1,dp);
+      
+      return dp[n-1][W] = max(steal,notsteal);
+    }
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+         vector<vector<int>> dp (500, vector<int> (500,-1));
+         return findKnapSack(W, wt, val, n, dp);
+    }
+};
